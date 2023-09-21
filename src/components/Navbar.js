16e119bf51c11ff9247384ad2682228c4afe19a8
Aman Choudhary <caman0987@gmail.com>
2023-09-21 21:30:32 +0530

    Updating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,9 @@ function Navbar(props) {
       <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
         <div className="container-fluid ">
           <Link className={`nav-link text-${props.mode === 'light' ? 'dark' : 'light'}`} style = {{fontSize: '30px'}} to ="/"><strong>{props.title}</strong></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
           <div className="collapse navbar-collapse mx-3" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -24,6 +27,43 @@ function Navbar(props) {
               <input className="form-check-input " style = {{marginTop: '8px'}} type="checkbox" onChange={props.toggleMode} aria-checked = "true" role="switch" id="flexSwitchCheckDefault" />
               <label className={`form-check-label text-${props.mode === 'light' ? 'dark' : 'light'} mx-3 fs-5`} htmlFor="flexSwitchCheckDefault">Enable dark mode</label>
             </div>
+
+            {/* <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
+  <div class="container-fluid">
+  <Link className={`nav-link text-${props.mode === 'light' ? 'dark' : 'light'}`} style = {{fontSize: '30px'}} to ="/"><strong>{props.title}</strong></Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav> */}
             
           </div>
         </div>
